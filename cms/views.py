3f907aa508a3f4c805c02ec4ef79b4c1c0af5271
Deepankar Arya <deepankar1994@gmail.com>
2015-12-03 15:31:44 +0530

    log parser added

--- cms/views.py
+++ cms/views.py
@@ -6,11 +6,23 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 import os
 import os.path
+import json
+#Parse the JSON log for KeyLogger data 
+def parseLogs(filePath):
+	f=open(filePath,'r')
+	result=''
+	for line in f:
+		j=json.loads(line)
+		if j['event_type']=='KeyLogger':
+			result+=line
+
+
+	return result
 
 def get_logs(request):
 	filePath='/edx/var/log/tracking/tracking.log'
 	if os.path.isfile(filePath) and os.access(filePath,os.R_OK):
-		return HttpResponse('tracking exist')
+		return HttpResponse(parseLogs(filePath))
 	else:
 		return HttpResponse('tracking does not')
 
