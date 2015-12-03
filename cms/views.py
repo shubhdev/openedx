3b7bf966e67a0312198779ccc53e6e47e5ceb8b0
Deepankar Arya <deepankar1994@gmail.com>
2015-12-03 16:54:53 +0530

    click download and log parsing updated

--- cms/views.py
+++ cms/views.py
@@ -5,24 +5,32 @@ from django.http import Http404
 from django.core.urlresolvers import reverse
 from django.views import generic
 import os
+from os import listdir
+from os.path import isfile, join
 import os.path
 import json
 #Parse the JSON log for KeyLogger data 
+
+
 def parseLogs(filePath):
-	f=open(filePath,'r')
-	result=''
+	f=open(filePath,'r')	
 	for line in f:
 		j=json.loads(line)
 		if j['event_type']=='KeyLogger':
 			result+=line
-
-
 	return result
 
 def get_logs(request):
-	filePath='/edx/var/log/tracking/tracking.log'
-	if os.path.isfile(filePath) and os.access(filePath,os.R_OK):
-		return HttpResponse(parseLogs(filePath))
-	else:
-		return HttpResponse('tracking does not')
+	fileDir='/edx/var/log/tracking/'
+	result=''
+	onlyfiles = [f for f in listdir(fileDir) if isfile(join(fileDir, f))]
+	for x in onlyfiles:
+		temp_file=fileDir+x
+		if(x.endswith('.log')):
+			result=result+parseLogs(temp_file)
+		else:
+			os.system('gunzip -c '+temp_file+' '+fileDir+'temp.log')
+			result=result+parseLogs(fileDir+'temp.log')
+			os.system('rm -f '+fileDir+'temp.log')
 
+	return HttpResponse(result)
